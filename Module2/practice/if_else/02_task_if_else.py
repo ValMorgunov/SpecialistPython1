3b015de4710bd07d50ce593578b89dbbedc2ac65
ValMorgunov <48404445+ValMorgunov@users.noreply.github.com>
2022-06-04 16:54:57 +0300

    Update 02_task_if_else.py

--- Module2/practice/if_else/02_task_if_else.py
+++ Module2/practice/if_else/02_task_if_else.py
@@ -2,4 +2,9 @@
 # Формат входных данных: Целое положительно число
 # Формат выходных данных: Если число оканчивается цифрой 5, вывести «YES», в противном случае — вывести «NO».
 
-# TODO: your code here
+welcome_msg = "Введите число: "
+answer = "NO"
+num = int(input(welcome_msg))
+if num%5 == 0 and num%10 != 0 :
+    answer = "YES"
+print(answer)
