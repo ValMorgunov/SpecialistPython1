9fe1a68daf0c420dc2396b1f56ac4bdd0aaae326
ValMorgunov <48404445+ValMorgunov@users.noreply.github.com>
2022-06-10 18:06:44 +0300

    Update 04_task_if_else.py

--- Module2/practice/if_else/04_task_if_else.py
+++ Module2/practice/if_else/04_task_if_else.py
@@ -4,4 +4,13 @@
 # а если делится на 3 и на 5 - вывести "Foobar".
 # Для всех остальных случаев не выводить ничего.
 
-# TODO: your code here
+welcome_msg = "Введите число: "
+number = int(input(welcome_msg))
+if number == 0:
+    quit()
+elif number % 15 == 0:
+    print("Foobar")
+elif number % 5 == 0:
+    print("Bar")
+elif number % 3 == 0:
+    print("Foo")
