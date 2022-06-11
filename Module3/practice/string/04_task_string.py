a8c543a7ed0b3c4aa54e4fb163c0878e6e07de4c
ValMorgunov <48404445+ValMorgunov@users.noreply.github.com>
2022-06-11 16:37:25 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -3,5 +3,14 @@
 
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
-
+l = text.count(" ")
+count = 0
+crop_text = text
 # TODO: your code here
+while l > 0:
+    l -= 1
+    index = crop_text.index(" ")
+    if index>5:
+        count+=1
+    crop_text=crop_text[index+1:]
+print(count)
