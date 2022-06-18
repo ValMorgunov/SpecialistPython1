3ce4f1fc21ec9ed5d0cde7a81a4808cbfaab781e
ValMorgunov <48404445+ValMorgunov@users.noreply.github.com>
2022-06-18 12:32:20 +0300

    Update 02_task_list.py

--- Module3/practice/list_and_tuple/02_task_list.py
+++ Module3/practice/list_and_tuple/02_task_list.py
@@ -2,5 +2,5 @@
 # Каждый элемент должен быть выведен с новой строки.
 
 fruits = ["яблоко", "банан", "киви", "ананас", "груша"]
-
-# TODO: your code here
+for fruit in fruits:
+    print(fruits.index(fruit)+1, fruit)
