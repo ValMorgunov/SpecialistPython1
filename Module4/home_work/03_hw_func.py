7d760f4175c1801ac1b2282c2c2419218d1e60ab
ValMorgunov <48404445+ValMorgunov@users.noreply.github.com>
2022-06-24 20:09:45 +0300

    Update 03_hw_func.py

--- Module4/home_work/03_hw_func.py
+++ Module4/home_work/03_hw_func.py
@@ -1,4 +1,14 @@
 # Даны координаты центров двух окружностей (x1; y1) (x2; y2) и и их радиусы  R1 и R2.
 # Находится ли одна окружность целиком внутри другой
 
-# TODO: your code here
+import random
+
+
+def check(x1, y1, x2, y2, r1, r2):
+    return (y1 - x1) ** 2 + (y2 - x2) ** 2 > (r2 - r1) ** 2
+
+
+x1, y1, x2, y2, r1, r2 = [random.randint(1, 30) for i in range(6)]
+print(check(x1, y1, x2, y2, r1, r2))
+print((y1 - x1) ** 2 + (y2 - x2) ** 2)
+print((r2 - r1) ** 2)
